73b6eca27d67e8f7a3efd493dad46c9854de2ffb
Nikanoras Zavadskij <nikanoras.zavadskij@mercell.com>
2021-12-27 17:52:55 +0200

    Add project files.

---
+++ TerraformAws/Program.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+Debug.WriteLine(app.Environment.EnvironmentName);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
